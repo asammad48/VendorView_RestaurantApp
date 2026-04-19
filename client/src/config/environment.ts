c28c7ba05d8defbe50043e2baac9515c17c36355
Abdul Sammad <Abdul Sammad@DESKTOP-L7JHC1H>
2026-04-19 23:03:29 +0500

    Changes in apiCalls

--- client/src/config/environment.ts
+++ client/src/config/environment.ts
@@ -8,7 +8,7 @@ export interface EnvironmentConfig {
 }
 
 // Development URLs
-const DEVELOPMENT_API_URL = 'https://3nbf18z7-44336.inc1.devtunnels.ms';
+const DEVELOPMENT_API_URL = 'https://localhost:44336';
 const QA_API_URL = 'https://restaurant-app-web-qa-001-eecdfsadcfgxevc9.centralindia-01.azurewebsites.net';
 
 // Get API base URL from environment variables or fallback logic
